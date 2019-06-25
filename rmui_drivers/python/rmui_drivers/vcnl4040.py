efcf48c914249c2b566f91ae6d11e34e2e524f17
Shingo Kitagawa <shingogo.5511@gmail.com>
2019-06-25 10:39:21 +0100

    fix typo

--- rmui_drivers/python/rmui_drivers/vcnl4040.py
+++ rmui_drivers/python/rmui_drivers/vcnl4040.py
@@ -3,17 +3,17 @@ import smbus2
 
 class VCNL4040(object):
 
-    def __init__(bus=1, address=0x60):
-        super(VNCL4040, self).__init__()
+    def __init__(self, bus=1, address=0x60):
+        super(VCNL4040, self).__init__()
         self.bus = smbus2.SMBus(bus)
         self.address = address
 
-    def start_blink():
+    def start_blink(self):
         self.bus.write_word_data(self.address, 0x03, 0x00)
 
-    def stop_blink():
+    def stop_blink(self):
         self.bus.write_word_data(self.address, 0x03, 0x01)
 
-    def read_proximity():
+    def read_proximity(self):
         data = self.bus.read_word_data(self.address, 0x08)
         return data
