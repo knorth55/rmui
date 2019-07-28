3c46c1ff3a73726b7092109ff8f7702b39dfae26
Shingo Kitagawa <shingogo.5511@gmail.com>
2019-07-28 11:04:52 +0100

    add ws281x python

--- rmui_drivers/python/rmui_drivers/__init__.py
+++ rmui_drivers/python/rmui_drivers/__init__.py
@@ -1,5 +1,8 @@
 from rmui_drivers import bno055  # NOQA
 from rmui_drivers import vcnl4040  # NOQA
+from rmui_drivers import ws281x  # NOQA
 
 from rmui_drivers.bno055 import BNO055  # NOQA
 from rmui_drivers.vcnl4040 import VCNL4040  # NOQA
+from rmui_drivers.ws281x import WS281x  # NOQA
+
---
+++ rmui_drivers/python/rmui_drivers/ws281x.py
@@ -0,0 +1,30 @@
+import time
+
+from rpi_ws281x import Color
+from rpi_ws281x import PixelStrip
+
+
+class WS281x(object):
+    hz = 800000
+    dma = 10
+    brightness = 127
+
+    def __init__(self, pin=10, n_led=1):
+        self.strip = .PixelStrip(
+            n_led, pin, self.hz, self.dma, False,
+            self.brightness, 0)
+        self.strip.begin()
+
+    def set_color(self, R, G, B):
+        color = Color(R, G, B)
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+
+    def turn_on(self, R, G, B, wait=0.01):
+        self.set_color(R, G, B)
+        time.sleep(wait)
+
+    def turn_off(self):
+        self.set_color(0, 0, 0)
+        time.sleep(0.01)
