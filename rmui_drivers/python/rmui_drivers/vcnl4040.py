8e9dcacbf49bddd573f4e255c8b2fa64a8289fa7
Shingo Kitagawa <shingogo.5511@gmail.com>
2020-03-17 12:06:07 +0000

    update vcnl4040

--- rmui_drivers/python/rmui_drivers/vcnl4040.py
+++ rmui_drivers/python/rmui_drivers/vcnl4040.py
@@ -7,13 +7,14 @@ class VCNL4040(object):
         super(VCNL4040, self).__init__()
         self.bus = smbus.SMBus(bus)
         self.address = address
+        self.bus.write_word_data(self.address, 0x00, 0x0001)
 
     def start_blink(self):
-        self.bus.write_word_data(self.address, 0x03, 0x00)
+        self.bus.write_word_data(self.address, 0x03, 0x080e)
 
     def stop_blink(self):
-        self.bus.write_word_data(self.address, 0x03, 0x01)
+        self.bus.write_word_data(self.address, 0x03, 0x0100)
 
     def read_proximity(self):
-        data = self.bus.read_word_data(self.address, 0x08)
+        data = self.bus.read_word_data(self.address, 0x0008)
         return data
