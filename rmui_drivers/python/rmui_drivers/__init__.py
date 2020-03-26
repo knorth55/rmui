e4f1e51755d0a719d02b9e2e7ab659b038ee7e22
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-03-27 00:52:00 +0900

    fix typo

--- rmui_drivers/python/rmui_drivers/__init__.py
+++ rmui_drivers/python/rmui_drivers/__init__.py
@@ -9,5 +9,5 @@ from rmui_drivers.bno055 import BNO055  # NOQA
 from rmui_drivers.pca9547 import PCA9547  # NOQA
 from rmui_drivers.rmui import RMUI  # NOQA
 from rmui_drivers.vcnl4040 import VCNL4040  # NOQA
-from rmui_drivers.vcnl4040_multiplexa import VCNL4040MultiPlexa  # NOQA
+from rmui_drivers.vcnl4040_multiplexa import VCNL4040Multiplexa  # NOQA
 from rmui_drivers.ws281x import WS281x  # NOQA
