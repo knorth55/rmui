7189d8294c1051075e1c39e6ba46947a5207d817
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-11-17 03:36:39 +0900

    add contact service for dummy rmui

--- rmui_drivers/node_scripts/dummy_rmui_node.py
+++ rmui_drivers/node_scripts/dummy_rmui_node.py
@@ -6,6 +6,8 @@ from rmui_drivers import DummyRMUI
 
 from force_proximity_ros.msg import ProximityArray
 from sensor_msgs.msg import Imu
+from std_srvs.srv import Empty
+from std_srvs.srv import EmptyResponse
 
 from rmui_msgs.msg import ImuCalibStatus
 
@@ -15,12 +17,13 @@ class DummyRMUINode(object):
     def __init__(self):
         frame_id = rospy.get_param('~frame_id', 'rmui_link')
         duration = rospy.get_param('~duration', 0.1)
-        n_board = rospy.get_param('~n_board', 6)
+        self.n_board = rospy.get_param('~n_board', 6)
         n_sensor = rospy.get_param('~n_sensor', 5)
         ea = rospy.get_param('~ea', 0.3)
         prx_threshold = rospy.get_param('~prx_threshold', 500)
 
-        self.device = DummyRMUI(n_board, n_sensor, ea, prx_threshold, frame_id)
+        self.device = DummyRMUI(
+            self.n_board, n_sensor, ea, prx_threshold, frame_id)
 
         self.pub_imu = rospy.Publisher(
             '~output/imu', Imu, queue_size=1)
@@ -30,6 +33,19 @@ class DummyRMUINode(object):
             '~output/proximities', ProximityArray, queue_size=1)
         self.timer = rospy.Timer(
             rospy.Duration(duration), self._timer_cb)
+        self.contact_services = []
+        self.release_services = []
+        for i in range(self.n_board):
+            self.contact_services.append(
+                rospy.Service(
+                    '~board{}/contact'.format(i), Empty,
+                    self._get_contact_service_cb(i)))
+            self.release_services.append(
+                rospy.Service(
+                    '~board{}/release'.format(i), Empty,
+                    self._get_release_service_cb(i)))
+        self.contact_reset_service = rospy.Service(
+            '~contact_reset', Empty, self._contact_reset_service_cb)
         rospy.loginfo('dummy rmui node initialized')
 
     def _timer_cb(self, event):
@@ -39,6 +55,32 @@ class DummyRMUINode(object):
         self.pub_imu.publish(imu_msg)
         self.pub_prx.publish(prx_msg)
         self.pub_imu_calib.publish(calib_msg)
+
+    def _get_contact_service_cb(self, i):
+        def _contact_cb(req):
+            return self._contact_service_cb(req, i)
+        return _contact_cb
+
+    def _get_release_service_cb(self, i):
+        def _release_cb(req):
+            return self._release_service_cb(req, i)
+        return _release_cb
+
+    def _contact_service_cb(self, req, i):
+        rospy.loginfo('board {} is contacted'.format(i))
+        self.device.contact_board(i)
+        return EmptyResponse()
+
+    def _release_service_cb(self, req, i):
+        rospy.loginfo('board {} is released'.format(i))
+        self.device.release_board(i)
+        return EmptyResponse()
+
+    def _contact_reset_service_cb(self, req):
+        rospy.loginfo('all board contact is resetted')
+        for i in range(self.n_board):
+            self.device.release_board(i)
+        return EmptyResponse()
 
 
 if __name__ == '__main__':
