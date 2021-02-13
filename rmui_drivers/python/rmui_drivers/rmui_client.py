dd639a9e33628ba2179db3cd706d82544836d9be
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-02-13 20:02:54 +0900

    add imu calibrated

--- rmui_drivers/python/rmui_drivers/rmui_client.py
+++ rmui_drivers/python/rmui_drivers/rmui_client.py
@@ -1,11 +1,21 @@
+import numpy as np
 try:
     import matplotlib.pyplot as plt
 except Exception:
     print('please install matplotlib if you use RMUIClient')
+try:
+    from scipy.spatial.transform import Rotation
+except Exception:
+    print('please install scipy for dummy rmui')
 
 import rospy
 
+from rmui_drivers import imu_utils
+
 from geometry_msgs.msg import Point
+from sensor_msgs.msg import Imu
+from std_srvs.srv import Trigger
+from std_srvs.srv import TriggerResponse
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
 
@@ -65,9 +75,71 @@ class RMUIClient(object):
         self.pub_prx_markers = rospy.Publisher(
             '{}/output/proximities/markers'.format(self.node_name),
             MarkerArray, queue_size=1)
+        self.pub_imu_calibrated = rospy.Publisher(
+            '{}/output/imu_calibrated'.format(self.node_name),
+            Imu, queue_size=1)
+        self.imu_calibrate_service = rospy.Service(
+            '{}/calibrate_imu'.format(self.node_name),
+            Trigger, self._calibrate_imu_service_cb)
         self.cm = plt.get_cmap('jet')
+        self.init_q = None
+        self.init_v = None
+        self.init_a = None
+
+    def _calibrate_imu_service_cb(self, req):
+        self.init_q = None
+        self.init_v = None
+        self.init_a = None
+        return TriggerResponse(success=True)
+
+    def _calibrate_imu(self, imu_msg):
+        try:
+            self.init_q = Rotation.from_quat([
+                imu_msg.orientation.x,
+                imu_msg.orientation.y,
+                imu_msg.orientation.z,
+                imu_msg.orientation.w
+            ])
+            self.init_v = np.array([
+                imu_msg.angular_velocity.x,
+                imu_msg.angular_velocity.y,
+                imu_msg.angular_velocity.z
+            ])
+            self.init_a = np.array([
+                imu_msg.linear_acceleration.x,
+                imu_msg.linear_acceleration.y,
+                imu_msg.linear_acceleration.z
+            ])
+        except ValueError as e:
+            rospy.logerr('{}'.format(e))
+            self.init_q = None
+            self.init_v = None
+            self.init_a = None
 
     def _cb(self, prx_msg, imu_msg, imu_calib_msg):
+        if self.init_q is None or self.init_v is None or self.init_a is None:
+            self._calibrate_imu(imu_msg)
+        try:
+            q = imu_msg.orientation
+            q = Rotation.from_quat([q.x, q.y, q.z, q.w])
+            q = (self.init_q.inv() * q).as_quat()
+        except ValueError as e:
+            rospy.logerr('{}'.format(e))
+            return
+
+        v = imu_msg.angular_velocity
+        v = np.array([v.x, v.y, v.z]) - self.init_v
+        a = imu_msg.linear_acceleration
+        a = np.array([a.x, a.y, a.z]) - self.init_a
+
+        imu_calibrated_msg = imu_utils.get_imu_msg(
+            q.tolist(), v.tolist(), a.tolist(),
+            imu_msg.orientation_covariance,
+            imu_msg.angular_velocity_covariance,
+            imu_msg.linear_acceleration_covariance)
+        imu_calibrated_msg.header = imu_msg.header
+        self.pub_imu_calibrated.publish(imu_calibrated_msg)
+
         header = prx_msg.header
         prx_markers = MarkerArray()
         for prx_id, (proximity, sensor_pos) in enumerate(
