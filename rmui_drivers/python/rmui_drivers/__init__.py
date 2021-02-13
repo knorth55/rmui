866d7ada26f1391b11ddec2683852ae64e1cd542
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-02-13 18:31:54 +0900

    do not import

--- rmui_drivers/python/rmui_drivers/__init__.py
+++ rmui_drivers/python/rmui_drivers/__init__.py
@@ -1,19 +1,19 @@
 from rmui_drivers import bno055  # NOQA
-from rmui_drivers import dummy_rmui  # NOQA
+# from rmui_drivers import dummy_rmui  # NOQA
 from rmui_drivers import imu_utils  # NOQA
 from rmui_drivers import pca9547  # NOQA
 from rmui_drivers import prx_utils  # NOQA
 from rmui_drivers import rmui  # NOQA
-from rmui_drivers import rmui_client  # NOQA
+# from rmui_drivers import rmui_client  # NOQA
 from rmui_drivers import vcnl4040  # NOQA
 from rmui_drivers import vcnl4040_multiplexa  # NOQA
 from rmui_drivers import ws281x  # NOQA
 
 from rmui_drivers.bno055 import BNO055  # NOQA
-from rmui_drivers.dummy_rmui import DummyRMUI  # NOQA
+# from rmui_drivers.dummy_rmui import DummyRMUI  # NOQA
 from rmui_drivers.pca9547 import PCA9547  # NOQA
 from rmui_drivers.rmui import RMUI  # NOQA
-from rmui_drivers.rmui_client import RMUIClient  # NOQA
+# from rmui_drivers.rmui_client import RMUIClient  # NOQA
 from rmui_drivers.vcnl4040 import VCNL4040  # NOQA
 from rmui_drivers.vcnl4040_multiplexa import VCNL4040Multiplexa  # NOQA
 from rmui_drivers.ws281x import WS281x  # NOQA
